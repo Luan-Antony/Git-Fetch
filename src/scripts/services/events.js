e48f1a200bcd7d7ee030f0143b06708fea975197
Luan Antony <115653362+Luan-Antony@users.noreply.github.com>
2024-08-01 15:59:49 -0300

    Update events.js
    added filter and slice to events return

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -2,8 +2,9 @@ import { baseUrl, evetnsQuantity } from '../variables.js';
 
 async function getEvents(userName) {
     const response = await fetch(`${baseUrl}/${userName}/events?per_page=${evetnsQuantity}`)
-    
-    return await response.json()
+    const events = await response.json()
+    return events.filter(element => element.type === 'CreateEvent' || element.type === 'PushEvent').slice(0, evetnsQuantity)
+
 }
 
 export { getEvents }
